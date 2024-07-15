fc07041d87486514ba1f5694746a349c840875d8
Ian Walton <51008396+ian-1@users.noreply.github.com>
2024-07-15 10:44:06 +0100

    Add IntendToSellController

---
+++ test/controllers/IntendToSellControllerSpec.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.http.Status
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class IntendToSellControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+
+  private val controller = new IntendToSellController(stubMessagesControllerComponents())
+
+  private val fakeRequest = FakeRequest("GET", "/intend-to-sell")
+
+  "GET /intend-to-sell" should {
+    "return 200" in {
+      val result = controller.show()(fakeRequest)
+
+      status(result) shouldBe Status.OK
+    }
+  }
+}
